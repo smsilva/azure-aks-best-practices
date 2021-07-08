b23a3d6ecb6081147df2070464cf41106f0d642c
Silvio Silva <smsilva@gmail.com>
2021-07-08 17:44:36 -0300

    wip

--- src/main.tf
+++ src/main.tf
@@ -16,6 +16,13 @@ module "bastions" {
   resource_group = module.resource_group.resource_group
 }
 
+module "virtual_machine" {
+  source         = "../modules/virtual-machine"
+  name           = "vm-1"
+  subnet_id      = module.subnets["snet-vm-example"].snet.id
+  resource_group = azurerm_resource_group.example
+}
+
 module "report_provision" {
   source = "./report_provision"
 }
