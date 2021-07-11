a26a416858a9e7b35feea7e3cd97e7268431e9b2
Silvio Silva <smsilva@gmail.com>
2021-07-11 10:13:53 -0300

    Network

--- modules/examples/network-peering/main.tf
+++ modules/examples/network-peering/main.tf
@@ -17,8 +17,8 @@ module "network" {
 
 module "newtork-peering" {
   source         = "../../network-peering"
-  first          = module.network.instances.hub0
-  second         = module.network.instances.spoke100
+  first          = lookup(module.network.instances, "hub0", null)
+  second         = lookup(module.network.instances, "spoke100", null)
   resource_group = azurerm_resource_group.example
 }
 
