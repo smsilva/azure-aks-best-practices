106032145cab45b5f0bc218fac21cac3a783030e
Silvio Silva <smsilva@gmail.com>
2021-07-13 13:58:27 -0300

    Versao do Provider

--- src/backend.tf
+++ src/backend.tf
@@ -1,9 +1,18 @@
 terraform {
+  required_version = ">= 1, < 2"
+
   backend "azurerm" {
     resource_group_name  = "iac"
     storage_account_name = "silvios"
     container_name       = "terraform"
     key                  = "azure-aks-best-practices.json"
+  }
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2, <3"
+    }
   }
 }
 
